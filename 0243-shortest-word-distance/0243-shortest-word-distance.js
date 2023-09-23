75eff390113fe39376c62c35d230565f35db38e8
Sabieh Ahmed <31431173+sabiehahmed10p@users.noreply.github.com>
2023-09-23 18:28:09 +0000

    Shortest Word Distance

---
+++ 0243-shortest-word-distance/0243-shortest-word-distance.js
@@ -0,0 +1,19 @@
+class Solution {
+    shortestDistance(words, word1, word2) {
+     let positionOne = -1;
+     let positionTwo = -1;
+     let shortestDistance = words.length;
+  
+     for (var i = 0; i < words.length; i++) {
+        if (words[i] === word1) positionOne = i;
+        if(words[i] === word2) positionTwo = i;
+  
+        if (positionOne !== -1 && positionTwo !== -1) {
+          shortestDistance = Math.min(shortestDistance, Math.abs(positionTwo - positionOne))
+        }
+  
+     }
+      return shortestDistance;
+    }
+  }
+  
